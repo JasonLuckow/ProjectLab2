94fd5f95ea26514766993b05f2a719adcb54909a
Jason Luckow <ijasonluck@gmail.com>
2020-09-14 22:41:39 -0500

    udpating

--- blink.py
+++ blink.py
@@ -1,20 +1,26 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
-ledpin = 32				# PWM pin connected to LED
-GPIO.setwarnings(False)			#disable warnings
-GPIO.setmode(GPIO.BOARD)		#set pin numbering system
-GPIO.setup(ledpin,GPIO.OUT)
-pi_pwm = GPIO.PWM(ledpin,1000)		#create PWM instance with frequency
-pi_pwm.start(0)				#start PWM of required Duty Cycle 
+# initialize variables/board
+ledpin = 32
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(ledpin, GPIO.OUT)
+
+# create PWM instance with frequency
+# frequency may need to be changed to mirror waveform generator
+pi_pwm = GPIO.PWM(ledpin, 1000) 
+pi_pwm.start(0)
+
 for i in range(100):
-    for duty in range(0,101,1):
-        duty = 50
-        pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
-        sleep(0.01)
-    sleep(0.5)
+    for duty in range(0, 101, 1):
+        duty = 50 # duty cycle is abritrarily chosen
+        pi_pwm.ChangeDutyCycle(duty)
+        sleep(.01)
+
+    # allows logic 1 to stay on for a while
+    sleep(.5)
     
-    for duty in range(100,-1,-1):
+    for duty in range(100, -1, -1):
         duty = 0
         pi_pwm.ChangeDutyCycle(duty)
         sleep(0.01)
@@ -22,14 +28,12 @@ for i in range(100):
 
 print("done")
 
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(24, GPIO.OUT)
-# print("hi")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.OUT)
+print("hi")
 
-# for i in range(100):
-#     GPIO.output(24, True)
-#     time.sleep(2)
-#     GPIO.output(24, False)
-#     time.sleep(2)
-
-# Create a PWM instance
+for i in range(100):
+    GPIO.output(24, True)
+    time.sleep(2)
+    GPIO.output(24, False)
+    time.sleep(2)
--- venv/src/main.py
+++ venv/src/main.py
@@ -1,6 +1,9 @@
-from src.model.Name import Name
+import RPi.GPIO as GPIO
 
-example = Name("Jason")
-name = example.getName()
-
-print(name)
+for i in range(10):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(24, GPIO.OUT)
+    GPIO.output(24, True)
+    time.sleep(1)
+    GPIO.output(24, False)
+    time.sleep(1)
