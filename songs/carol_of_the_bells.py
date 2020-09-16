081d392473efb0323d511f85b99ab1a1f91dcc59
Jason Luckow <ijasonluck@gmail.com>
2020-09-16 01:32:12 -0500

    new version

--- songs/carol_of_the_bells.py
+++ songs/carol_of_the_bells.py
@@ -10,13 +10,14 @@ class NewCarolSong():
     def startsong(self):
         print("Carol button was clicked")
         self.win.updatelabel2("You clicked: Carol of the Bells")
+        self.app.processEvents()
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(24, GPIO.OUT)
         print("hi")
 
         for i in range(5):
-            self.app.processEvents()
             self.win.updatelabel2("You clicked: Carol of the Bells {}".format(i))
+            self.app.processEvents()
             GPIO.output(24, True)
             sleep(1)
             GPIO.output(24, False)
