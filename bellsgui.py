ad382b14696da3147756102fc152241731a5533f
Jason Luckow <ijasonluck@gmail.com>
2020-09-26 14:35:42 -0500

    new version

--- bellsgui.py
+++ bellsgui.py
@@ -106,7 +106,7 @@ class MyWindow(QMainWindow):
 
         carolsong = carol.NewCarolSong(self.win, self.app)
 
-        worker = Worker(self.carolsong.startsong) # Any other args, kwargs are passed to the run function
+        worker = Worker(carolsong.startsong) # Any other args, kwargs are passed to the run function
         # worker.signals.result.connect(self.print_output)
         # worker.signals.finished.connect(self.thread_complete)
         worker.signals.progress.connect(self.carolnotify)
