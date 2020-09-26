5ea0865c53fcad18b31daa47a30b343abe09877c
Jason Luckow <ijasonluck@gmail.com>
2020-09-26 11:27:43 -0500

    new version

--- threadpractice.py
+++ threadpractice.py
@@ -57,7 +57,7 @@ x.start()
 y = threading.Thread(target=calc, args=(True, 24, .5,))
 y.start()
 
-z = threading.Thread(target=calc, args=(False, 25, .75,))
+z = threading.Thread(target=calc, args=(False, 25, 1,))
 z.start()
 
 x.join()
