99e5c395a7a0432ef69011bca5ad9d216368bcef
Jason Luckow <ijasonluck@gmail.com>
2020-09-26 12:09:24 -0500

    new version

--- songs/carol_of_the_bells.py
+++ songs/carol_of_the_bells.py
@@ -9,6 +9,12 @@ class NewCarolSong():
         self.app = app
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(23, GPIO.OUT)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(23, GPIO.OUT)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(24, GPIO.OUT)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(25, GPIO.OUT)
 
     def startsong(self):
         print("Carol button was clicked")
@@ -17,10 +23,63 @@ class NewCarolSong():
         for i in range(2):
             self.win.updatelabel2(" You clicked: Carol of the Bells.\nIteration {}".format(i + 1))
             self.app.processEvents()
-            GPIO.output(23, True)
-            sleep(.5)
-            GPIO.output(23, False)
-            sleep(.5)
+            # GPIO.output(23, True)
+            # sleep(.5)
+            # GPIO.output(23, False)
+            # sleep(.5)
+
+            x = threading.Thread(target=calc, args=(True, 23, .5, 25,))
+            x.start()
+
+            y = threading.Thread(target=calc, args=(True, 24, .5, 25,))
+            y.start()
+
+            z = threading.Thread(target=calc, args=(False, 25, 1, 12,))
+            z.start()
+
+            x.join()
+            y.join()
+            z.join()
+
+            all(False)s
 
         print("done")
         self.win.updatelabel2("Carol button was clicked.\nClick another!")
+
+    
+
+    def motorswitch(bo, pin, t):
+        GPIO.output(pin, bo)
+        time.sleep(t)
+
+    arr = []
+
+    def timenow():
+        return (datetime.now().strftime("%H:%M:%S"))
+
+    def calc(bo, pin, t, n):
+        for i in range(n):
+            bo = not bo
+            GPIO.output(pin, bo)
+            time.sleep(t)
+
+
+
+    def stepsame(count, type, bo, t):
+        print("working {}".format(count))
+        arr.append([count, type, bo, timenow()])
+        time.sleep(t)
+
+    def all(bo):
+        x = threading.Thread(target=motorswitch, args=(bo, 23, .5,))
+        x.start()
+
+        y = threading.Thread(target=motorswitch, args=(bo, 24, .5,))
+        y.start()
+
+        z = threading.Thread(target=motorswitch, args=(bo, 25, .5,))
+        z.start()
+
+        x.join()
+        y.join()
+        z.join()
