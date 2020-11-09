f33a2f86a364f551ae7fc752ba89093f66ffd464
JasonLuckow <ijasonluck@gmail.com>
2020-11-08 22:58:17 -0600

    Fixed

--- NickCode/Housetop.py
+++ NickCode/Housetop.py
@@ -1,208 +1,217 @@
 import threading
 import time
 import RPi.GPIO as GPIO
-import music
+import music1
 import math
 
-b = 1
-a = 2
-g = 3
-e = 7 
-A = 4
-f = 3
-bF = 8
-d = 13
-c = 14
+bF = 14
+b = 24
+c = 16
+d = 25
+dS = 4
+e = 7
+f = 19
+fS = 17
+g = 8
+A = 12
+BF = 22
+B = 20
+C = 18
+D = 15
+DS = 13
+E = 1
+F = 6
+G = 21
 
-music.tempo = music.updateTempo(140)
+#music1.tempo = music1.updateTempo(140)
 
 def melody():
   #m1
-  music.qNote(g)
-  music.eNote(g)
-  music.eNote(A)
-  music.qNote(g)
-  music.qNote(e)
+  music1.qNote(g)
+  music1.eNote(g)
+  music1.eNote(A)
+  music1.qNote(g)
+  music1.qNote(e)
 
   #m2s
-  music.qNote(c)
-  music.qNote(e)
-  music.hNote(g)
+  music1.qNote(c)
+  music1.qNote(e)
+  music1.hNote(g)
 
   #m3
-  music.qNote(A)
-  music.qNote(A)
-  music.qNote(g)
-  music.qNote(e)
+  music1.qNote(A)
+  music1.qNote(A)
+  music1.qNote(g)
+  music1.qNote(e)
 
   #m4
-  music.qNote(d)
-  music.qNote(g)
-  music.hNote(g)
+  music1.qNote(d)
+  music1.qNote(g)
+  music1.hNote(g)
   time.sleep(.1)
 
   #m5
-  music.qNote(g)
-  music.eNote(g)
-  music.eNote(A)
-  music.qNote(g)
-  music.eNote(e)
-  music.eNote(d)
+  music1.qNote(g)
+  music1.eNote(g)
+  music1.eNote(A)
+  music1.qNote(g)
+  music1.eNote(e)
+  music1.eNote(d)
 
   #m6
-  music.qNote(c)
-  music.qNote(e)
-  music.hNote(g)
+  music1.qNote(c)
+  music1.qNote(e)
+  music1.hNote(g)
 
   #m7
-  music.qNote(A)
-  music.eNote(A)
-  music.eNote(A)
-  music.eNote(g)
-  music.eNote(g)
-  music.qNote(e)
+  music1.qNote(A)
+  music1.eNote(A)
+  music1.eNote(A)
+  music1.eNote(g)
+  music1.eNote(g)
+  music1.qNote(e)
 
   #m8
-  music.qNote(d)
-  music.qNote(g)
-  music.hNote(c)
+  music1.qNote(d)
+  music1.qNote(g)
+  music1.hNote(c)
   time.sleep(.1)
 
   #m9
-  music.qNote(f)
-  music.qNote(f)
-  music.hNote(A)
+  music1.qNote(f)
+  music1.qNote(f)
+  music1.hNote(A)
 
   #m10
-  music.qNote(g)
-  music.eNote(g)
-  music.eNote(g)
-  music.hNote(e)
+  music1.qNote(g)
+  music1.eNote(g)
+  music1.eNote(g)
+  music1.hNote(e)
 
   #m11
-  music.qNote(d)
-  music.qNote(d)
-  music.hNote(f)
+  music1.qNote(d)
+  music1.qNote(d)
+  music1.hNote(f)
 
   #m12
-  music.qNote(e)
-  music.eNote(g)
-  music.eNote(g)
-  music.qNote(c)
-  music.qNote(e)
+  music1.qNote(e)
+  music1.eNote(g)
+  music1.eNote(g)
+  music1.qNote(c)
+  music1.qNote(e)
 
   #m13
-  music.qNote(g)
-  music.eNote(g)
-  music.eNote(A)
-  music.qNote(g)
-  music.qNote(e)
+  music1.qNote(g)
+  music1.eNote(g)
+  music1.eNote(A)
+  music1.qNote(g)
+  music1.qNote(e)
 
   #m14
-  music.qNote(f)
-  music.qNote(g)
-  music.qNote(A)
-  music.qNoteRest()
+  music1.qNote(f)
+  music1.qNote(g)
+  music1.qNote(A)
+  music1.qNoteRest()
 
   #m15
-  music.qNote(g)
-  music.eNote(g)
-  music.eNote(A)
-  music.qNote(g)
-  music.eNote(e)
-  music.eNote(e)
+  music1.qNote(g)
+  music1.eNote(g)
+  music1.eNote(A)
+  music1.qNote(g)
+  music1.eNote(e)
+  music1.eNote(e)
 
   #m16
-  music.qNote(d)
-  music.qNote(g)
-  music.hNote(c)
+  music1.qNote(d)
+  music1.qNote(g)
+  music1.hNote(c)
   
 
 def bass():
   #m1
-  music.hNote(c)
+  music1.hNote(c)
   time.sleep(.2)
-  music.hNote(g)
+  music1.hNote(g)
   time.sleep(.1)
 
   #m2
-  music.hNote(c)
+  music1.hNote(c)
   time.sleep(.1)
-  music.hNote(e)
+  music1.hNote(e)
 
   #m3
-  music.hNote(f)
+  music1.hNote(f)
   time.sleep(.1)
-  music.hNote(e)
+  music1.hNote(e)
   time.sleep(.1)
 
   #m4
-  music.qNote(b)
-  music.qNote(a)
-  music.qNote(g)
-  music.qNote(b)
+  music1.qNote(b)
+  music1.qNote(A)
+  music1.qNote(g)
+  music1.qNote(b)
 
   #m5
-  music.hNote(c)
+  music1.hNote(c)
   time.sleep(.2)
-  music.hNote(g)
+  music1.hNote(g)
   time.sleep(.2)
 
   #m6
-  music.hNote(c)
+  music1.hNote(c)
   time.sleep(.1)
-  music.hNote(e)
+  music1.hNote(e)
 
   #m7
-  music.hNote(f)
+  music1.hNote(f)
   time.sleep(.2)
-  music.hNote(e)
+  music1.hNote(e)
   time.sleep(.2)
 
   #m8
-  music.hNote(f)
+  music1.hNote(f)
   time.sleep(.1)
-  music.qNote(e)
-  music.qNote(c)
+  music1.qNote(e)
+  music1.qNote(c)
 
   #m9
-  music.wNote(f)
+  music1.wNote(f)
   time.sleep(.2)
 
   #m10
-  music.wNote(e)
+  music1.wNote(e)
   time.sleep(.3)
 
   #m11
-  music.wNote(d)
+  music1.wNote(d)
   time.sleep(.2)
 
   #m12
-  music.wNote(c)
+  music1.wNote(c)
   time.sleep(.4)
 
   #m13
-  music.hNote(c)
+  music1.hNote(c)
   time.sleep(.2)
-  music.hNote(bF)
-  time.sleep(.2)
+  music1.hNote(bF)
+  time.sleep(.1)
 
   #m14
-  music.qNote(A)
-  music.qNote(g)
-  music.qNote(f)
-  music.qNoteRest()
+  music1.qNote(A)
+  music1.qNote(g)
+  music1.qNote(f)
+  music1.qNoteRest()
   
   #m15
-  music.hNote(c)
+  music1.hNote(c)
   time.sleep(.2)
-  music.hNote(g)
+  music1.hNote(g)
   time.sleep(.2)
 
   #m16
-  music.hNote(b)
+  music1.hNote(b)
   time.sleep(.1)
-  music.hNote(c)
+  music1.hNote(c)
 
 # Setting up threads and starting them
 high = threading.Thread(target=melody)
